9c51d3376e85f70e3d5462f7d5cf4367b1c86d26
katamnesis <k.pongrac@web.de>
2020-05-31 12:16:35 +0200

    corrected title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Katas Little Texteditor");
+        primaryStage.setTitle("Katas Little Text Editor");
         primaryStage.setScene(new Scene(root, 600, 410));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
